b62fbf98f807f4520baf4c6f3445561e80e3efa5
Lucas Nelaupe <lucas.nelaupe@vcube.co.jp>
2016-07-27 17:04:21 +0800

    Add prefix to crop param

--- library/src/cropkit/java/fr/nelaupe/unicrop/CropKitParams.java
+++ library/src/cropkit/java/fr/nelaupe/unicrop/CropKitParams.java
@@ -9,43 +9,51 @@ import android.os.Bundle;
  */
 public final class CropKitParams
 {
+    private static final String PREFIX = "CROP_KIT_PARAMS_";
+
     public int aspectX;
+    private static final String ASPECT_X = PREFIX + "aspectX";
 
     public int aspectY;
+    private static final String ASPECT_Y = PREFIX + "aspectY";
 
     public boolean detectFace;
+    private static final String DETECT_FACE = PREFIX + "detectFace";
 
     public Uri inputUri;
+    private static final String INPUT_URI = PREFIX + "inputUri";
 
     public Uri outputUri;
+    private static final String OUPUT_URI = PREFIX + "outputUri";
 
     public Bitmap.CompressFormat format;
+    private static final String FORMAT = PREFIX + "format";
 
     public float[] defaultCropPosition;
+    private static final String DEFAULT_CROP_POSITION = PREFIX + "defaultCropPosition";
 
     public Bundle create() {
         Bundle bundle = new Bundle();
-        bundle.putInt("aspectX", aspectX);
-        bundle.putInt("aspectY", aspectY);
-        bundle.putBoolean("faceDetection", detectFace);
-        bundle.putParcelable("input", inputUri);
-        bundle.putParcelable("output", outputUri);
-        bundle.putSerializable("format", format);
-        bundle.putFloatArray("defaultCropArea" , defaultCropPosition);
+        bundle.putInt(ASPECT_X, aspectX);
+        bundle.putInt(ASPECT_Y, aspectY);
+        bundle.putBoolean(DETECT_FACE, detectFace);
+        bundle.putParcelable(INPUT_URI, inputUri);
+        bundle.putParcelable(OUPUT_URI, outputUri);
+        bundle.putSerializable(FORMAT, format);
+        bundle.putFloatArray(DEFAULT_CROP_POSITION , defaultCropPosition);
         return bundle;
     }
 
     public static CropKitParams restore(Bundle bundle) {
         CropKitParams cropKitParams = new CropKitParams();
-        cropKitParams.aspectX = bundle.getInt("aspectX", 1);
-        cropKitParams.aspectY = bundle.getInt("aspectY", 1);
-        cropKitParams.detectFace = bundle.getBoolean("faceDetection", false);
-        cropKitParams.inputUri = bundle.getParcelable("input");
-        cropKitParams.outputUri = bundle.getParcelable("output");
-        cropKitParams.format = bundle.containsKey("format") ? (Bitmap.CompressFormat) bundle.getSerializable("format") : Bitmap.CompressFormat.JPEG;
-        cropKitParams.defaultCropPosition = bundle.getFloatArray("defaultCropArea");
+        cropKitParams.aspectX = bundle.getInt(ASPECT_X, 1);
+        cropKitParams.aspectY = bundle.getInt(ASPECT_Y, 1);
+        cropKitParams.detectFace = bundle.getBoolean(DETECT_FACE, false);
+        cropKitParams.inputUri = bundle.getParcelable(INPUT_URI);
+        cropKitParams.outputUri = bundle.getParcelable(OUPUT_URI);
+        cropKitParams.format = bundle.containsKey(FORMAT) ? (Bitmap.CompressFormat) bundle.getSerializable(FORMAT) : Bitmap.CompressFormat.JPEG;
+        cropKitParams.defaultCropPosition = bundle.getFloatArray(DEFAULT_CROP_POSITION);
         return cropKitParams;
     }
-
 
 }
