a7fc9f3d18751367cb80b5282bd18c7e74be5e01
Yunarta Kartawahyudi <yunarta@vcube.co.jp>
2018-03-13 14:57:14 +0800

    VG-5739　Removing black square during cropping photo for avatar or group photo

--- library/src/cropkit/java/fr/nelaupe/unicrop/CropImageView.java
+++ library/src/cropkit/java/fr/nelaupe/unicrop/CropImageView.java
@@ -353,10 +353,10 @@ public class CropImageView extends ImageViewTouchBase {
         Rect imageRect = new Rect(0, 0, width, height);
 
         // make the default size about 4/5 of the width or height
-        int cropWidth = width; // Math.min(width, height) * 4 / 5;
+        int cropWidth = Math.min(width, height); // * 4 / 5;
 
         //noinspection SuspiciousNameCombination
-        int cropHeight = height; // cropWidth;
+        int cropHeight = cropWidth;
 
         if (mAspectX != 0 && mAspectY != 0) {
             if (mAspectX > mAspectY) {
